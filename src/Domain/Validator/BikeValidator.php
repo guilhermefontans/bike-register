dbc6c03c12bd003a0e6ab662a119301001b72e9c
Guilherme Fontans <guilherme.fontans@gmail.com>
2020-10-21 21:46:04 +0000

    Adição de validator para todos campos

--- src/Domain/Validator/BikeValidator.php
+++ src/Domain/Validator/BikeValidator.php
@@ -19,10 +19,57 @@ class BikeValidator implements ValidatorInterface
     public function __construct()
     {
         $this->validator = new Validator();
-        $this->validator->addRule(Validator::key('description', Validator::allOf(
-            Validator::notEmpty()->setTemplate('A descrição deve ser informada'),
-            Validator::length(3, 24)->setTemplate('Tamanho de descrição inválida')
-        ))->setTemplate('O campo "description" é obrigatório'));
+        $this->validator->addRule(
+            Validator::key(
+                'descricao', Validator::allOf(
+                    Validator::notEmpty()->setTemplate('The description must not be empty'),
+                    Validator::length(3, 255)->setTemplate('Invalid length')
+                )
+            )->setTemplate("The key 'descricao' is required")
+        );
+
+        $this->validator->addRule(
+            Validator::key(
+                'modelo', Validator::allOf(
+                    Validator::notEmpty()->setTemplate('The model must not be empty'),
+                    Validator::length(1, 100)->setTemplate('Invalid length')
+                )
+            )->setTemplate("The key 'modelo' is required")
+        );
+
+        $this->validator->addRule(
+            Validator::key(
+                'nome-comprador', Validator::allOf(
+                    Validator::notEmpty()->setTemplate('The nome-comprador must not be empty'),
+                    Validator::length(2, 50)->setTemplate('Invalid length')
+                )
+            )->setTemplate("The key 'nome-comprador' is required")
+        );
+
+        $this->validator->addRule(
+            Validator::key(
+                'nome-loja', Validator::allOf(
+                    Validator::notEmpty()->setTemplate('The nome-loja must not be empty'),
+                    Validator::length(3, 255)->setTemplate('Invalid length')
+                )
+            )->setTemplate("The key 'nome-loja' is required")
+        );
+
+        $this->validator->addRule(
+            Validator::key(
+                'data-compra', Validator::allOf(
+                Validator::date('Y-m-d')->setTemplate('Invalid date')
+            )
+            )->setTemplate("The key 'data-compra' is required")
+        );
+
+        $this->validator->addRule(
+            Validator::key(
+                'preco', Validator::allOf(
+                    Validator::floatVal()->setTemplate('Price must be a float')
+                )
+            )->setTemplate("The key 'preco' is required")
+        );
     }
 
     public function validate(array $data)
